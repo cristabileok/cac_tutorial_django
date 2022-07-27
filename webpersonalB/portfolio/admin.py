5ae22bb78aaedd357099eefe4ce119cb4ee919d7
Cristian <stabilecristian@gmail.com>
2022-07-26 22:15:30 -0300

    Added utilities to Admin Pannel

--- webpersonalB/portfolio/admin.py
+++ webpersonalB/portfolio/admin.py
@@ -5,5 +5,9 @@ from .models import Project
 
 class ProjectAdmin(admin.ModelAdmin):
     readonly_fields=('created','updated')
+    list_display=('title','updated','created')
+    search_fields=("title","created","updated")
+    list_filter=('updated',)
+    date_hierarchy="updated"
 
 admin.site.register(Project, ProjectAdmin)
